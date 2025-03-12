3ea94a83f9cac99832a946e5b2e695fe67d1ea37
Junhao Hu <derekhjh9@gmail.com>
2025-03-12 21:38:38 +0800

    Init Eye

---
+++ Eye/op_host/eye.cpp
@@ -0,0 +1,66 @@
+
+#include "eye_tiling.h"
+#include "register/op_def_registry.h"
+
+
+namespace optiling {
+static ge::graphStatus TilingFunc(gert::TilingContext* context)
+{
+
+  EyeTilingData tiling;
+  const gert::StorageShape* x1_shape = context->GetInputShape(0);
+  int32_t data_sz = 1;
+  for (int i = 0; i < x1_shape->GetStorageShape().GetDimNum(); i++)
+    data_sz *= x1_shape->GetStorageShape().GetDim(i);
+  tiling.set_size(data_sz);
+  context->SetBlockDim(8);
+  tiling.SaveToBuffer(context->GetRawTilingData()->GetData(), context->GetRawTilingData()->GetCapacity());
+  context->GetRawTilingData()->SetDataSize(tiling.GetDataSize());
+
+  return ge::GRAPH_SUCCESS;
+}
+}
+
+
+namespace ge {
+static ge::graphStatus InferShape(gert::InferShapeContext* context)
+{
+    const gert::Shape* x1_shape = context->GetInputShape(0);
+    gert::Shape* y_shape = context->GetOutputShape(0);
+    *y_shape = *x1_shape;
+    return GRAPH_SUCCESS;
+}
+}
+
+
+namespace ops {
+class Eye : public OpDef {
+public:
+    explicit Eye(const char* name) : OpDef(name)
+    {
+        this->Input("y")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT, ge::DT_FLOAT16, ge::DT_INT32, ge::DT_DOUBLE})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+        this->Output("y")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT, ge::DT_FLOAT16, ge::DT_INT32, ge::DT_DOUBLE})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+        this->Attr("num_rows").Int();
+        this->Attr("num_columns").AttrType(OPTIONAL).Int(0);
+        this->Attr("batch_shape").AttrType(OPTIONAL).ListInt({});
+        this->Attr("dtype").AttrType(OPTIONAL).Int(0);
+
+        this->SetInferShape(ge::InferShape);
+
+        this->AICore()
+            .SetTiling(optiling::TilingFunc);
+        this->AICore().AddConfig("ascend910b");
+
+    }
+};
+
+OP_ADD(Eye);
+}
