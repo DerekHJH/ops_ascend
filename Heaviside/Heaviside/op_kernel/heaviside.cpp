3b379f239ecc61aab4f2098bfa5c2610dc4a8ea6
Junhao Hu <derekhjh9@gmail.com>
2025-03-14 15:05:08 +0800

    Half way

--- Heaviside/Heaviside/op_kernel/heaviside.cpp
+++ Heaviside/Heaviside/op_kernel/heaviside.cpp
@@ -1,26 +1,28 @@
 #include "kernel_operator.h"
-
+constexpr int32_t BUFFER_NUM = 2;
 class Heaviside {
 public:
     __aicore__ inline Heaviside() {}
     __aicore__ inline void Init(GM_ADDR x, GM_ADDR y, GM_ADDR z, GM_ADDR tiling)
     {
         GET_TILING_DATA(tiling_data, tiling);
-
-        this->blockLength = totalLength / AscendC::GetBlockNum();
-        this->tileNum = tileNum;
-        this->tileLength = this->blockLength / tileNum / BUFFER_NUM;
-
-        xGm.SetGlobalBuffer((__gm__ DTYPE_X *)x + this->blockLength * AscendC::GetBlockIdx(), this->blockLength);
-        yGm.SetGlobalBuffer((__gm__ DTYPE_Y *)y + this->blockLength * AscendC::GetBlockIdx(), this->blockLength);
-        zGm.SetGlobalBuffer((__gm__ DTYPE_Z *)z + this->blockLength * AscendC::GetBlockIdx(), this->blockLength);
-        pipe.InitBuffer(inQueueX, BUFFER_NUM, this->tileLength * sizeof(DTYPE_X));
-        pipe.InitBuffer(inQueueY, BUFFER_NUM, this->tileLength * sizeof(DTYPE_Y));
-        pipe.InitBuffer(outQueueZ, BUFFER_NUM, this->tileLength * sizeof(DTYPE_Z));
+        this->num_elements_total = tiling_data.num_elements_total;
+        this->num_elements_per_core = tiling_data.num_elements_per_core;
+        this->num_tiles = tiling_data.num_tiles;
+        this->num_elements_per_tile = tiling_data.num_elements_per_tile;
+        this->num_elements_per_buffer = tiling_data.num_elements_per_buffer;
+        uint32_t start_address = this->num_elements_per_core * AscendC::GetBlockIdx();
+        uint32_t num_real_elements = min(this->num_elements_per_core, this->num_elements_total - start_address);
+        xGm.SetGlobalBuffer((__gm__ DTYPE_X *)x + start_address, num_real_elements);
+        yGm.SetGlobalBuffer((__gm__ DTYPE_Y *)y + start_address, num_real_elements);
+        zGm.SetGlobalBuffer((__gm__ DTYPE_Z *)z + start_address, num_real_elements);
+        pipe.InitBuffer(inQueueX, BUFFER_NUM, this->num_elements_per_buffer * sizeof(DTYPE_X));
+        pipe.InitBuffer(inQueueY, BUFFER_NUM, this->num_elements_per_buffer * sizeof(DTYPE_Y));
+        pipe.InitBuffer(outQueueZ, BUFFER_NUM, this->num_elements_per_buffer * sizeof(DTYPE_Z));
     }
     __aicore__ inline void Process()
     {
-        int32_t loopCount = this->tileNum * BUFFER_NUM;
+        int32_t loopCount = this->num_tiles * BUFFER_NUM;
         for (int32_t i = 0; i < loopCount; i++) {
             CopyIn(i);
             Compute(i);
@@ -33,8 +35,9 @@ private:
     {
         AscendC::LocalTensor<DTYPE_X> xLocal = inQueueX.AllocTensor<DTYPE_X>();
         AscendC::LocalTensor<DTYPE_Y> yLocal = inQueueY.AllocTensor<DTYPE_Y>();
-        AscendC::DataCopy(xLocal, xGm[progress * this->tileLength], this->tileLength);
-        AscendC::DataCopy(yLocal, yGm[progress * this->tileLength], this->tileLength);
+        uint32_t
+        AscendC::DataCopy(xLocal, xGm[progress * this->num_elements_per_buffer], this->num_elements_per_buffer);
+        AscendC::DataCopy(yLocal, yGm[progress * this->num_elements_per_buffer], this->num_elements_per_buffer);
         inQueueX.EnQue(xLocal);
         inQueueY.EnQue(yLocal);
     }
@@ -43,7 +46,7 @@ private:
         AscendC::LocalTensor<DTYPE_X> xLocal = inQueueX.DeQue<DTYPE_X>();
         AscendC::LocalTensor<DTYPE_Y> yLocal = inQueueY.DeQue<DTYPE_Y>();
         AscendC::LocalTensor<DTYPE_Z> zLocal = outQueueZ.AllocTensor<DTYPE_Z>();
-        AscendC::Add(zLocal, xLocal, yLocal, this->tileLength);
+        AscendC::Add(zLocal, xLocal, yLocal, this->num_elements_per_buffer);
         outQueueZ.EnQue<DTYPE_Z>(zLocal);
         inQueueX.FreeTensor(xLocal);
         inQueueY.FreeTensor(yLocal);
@@ -51,7 +54,7 @@ private:
     __aicore__ inline void CopyOut(int32_t progress)
     {
         AscendC::LocalTensor<DTYPE_Z> zLocal = outQueueZ.DeQue<DTYPE_Z>();
-        AscendC::DataCopy(zGm[progress * this->tileLength], zLocal, this->tileLength);
+        AscendC::DataCopy(zGm[progress * this->num_elements_per_buffer], zLocal, this->num_elements_per_buffer);
         outQueueZ.FreeTensor(zLocal);
     }
 
@@ -62,11 +65,12 @@ private:
     AscendC::GlobalTensor<DTYPE_X> xGm;
     AscendC::GlobalTensor<DTYPE_Y> yGm;
     AscendC::GlobalTensor<DTYPE_Z> zGm;
-    num_elements_per_core
+    uint32_t num_elements_total;
+    uint32_t num_elements_per_core;
+    uint32_t num_tiles;
+    uint32_t num_elements_per_tile;
+    uint32_t num_elements_per_buffer;
 };
-
-
-
 
 extern "C" __global__ __aicore__ void heaviside(GM_ADDR x, GM_ADDR y, GM_ADDR z, GM_ADDR workspace, GM_ADDR tiling) {
     Heaviside op;
