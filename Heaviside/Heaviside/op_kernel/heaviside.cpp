eaebd85d6ecbec822d58183cceb7c31c3876e0a3
Junhao Hu <derekhjh9@gmail.com>
2025-03-19 17:51:56 +0800

    Debug

--- Heaviside/Heaviside/op_kernel/heaviside.cpp
+++ Heaviside/Heaviside/op_kernel/heaviside.cpp
@@ -11,10 +11,10 @@ public:
         this->num_tiles = tiling_data.num_tiles;
         this->num_elements_per_tile = tiling_data.num_elements_per_tile;
         uint32_t start_idx = this->num_elements_per_core * AscendC::GetBlockIdx();
-        uint32_t num_real_elements = min(this->num_elements_per_core, this->num_elements_total - start_idx);
-        xGm.SetGlobalBuffer((__gm__ DTYPE_X *)x + start_idx, num_real_elements);
-        yGm.SetGlobalBuffer((__gm__ DTYPE_Y *)y + start_idx, num_real_elements);
-        zGm.SetGlobalBuffer((__gm__ DTYPE_Z *)z + start_idx, num_real_elements);
+        this->num_real_elements_per_core = max(0, min(this->num_elements_per_core, this->num_elements_total - start_idx));
+        xGm.SetGlobalBuffer((__gm__ DTYPE_X *)x + start_idx, this->num_real_elements_per_core);
+        yGm.SetGlobalBuffer((__gm__ DTYPE_Y *)y + start_idx, this->num_real_elements_per_core);
+        zGm.SetGlobalBuffer((__gm__ DTYPE_Z *)z + start_idx, this->num_real_elements_per_core);
         pipe.InitBuffer(inQueueX, BUFFER_NUM, this->num_elements_per_tile * sizeof(DTYPE_X));
         pipe.InitBuffer(inQueueY, BUFFER_NUM, this->num_elements_per_tile * sizeof(DTYPE_Y));
         pipe.InitBuffer(outQueueZ, BUFFER_NUM, this->num_elements_per_tile * sizeof(DTYPE_Z));
@@ -23,29 +23,32 @@ public:
     __aicore__ inline void Process()
     {
         uint32_t start_idx;
-        uint32_t num_real_elements;
+        uint32_t num_real_elements_per_tile;
         for (int32_t i = 0; i < this->num_tiles; i++) {
             start_idx = i * this->num_elements_per_tile;
-            num_real_elements = min(this->num_elements_per_tile, this->num_elements_total - start_idx);
+            num_real_elements_per_tile = min(this->num_elements_per_tile, this->num_real_elements_per_core - start_idx);
+            if(num_real_elements_per_tile <= 0) {
+                break; // All that is left are extra elements.
+            }
             if (AscendC::GetBlockIdx() == 0)
-                AscendC::printf("start_idx: %u, num_real_elements: %u\n", start_idx, num_real_elements);
-            CopyIn(i, start_idx, num_real_elements);
-            Compute(i, start_idx, num_real_elements);
-            CopyOut(i, start_idx, num_real_elements);
+                AscendC::printf("start_idx: %u, num_real_elements_per_tile: %u\n", start_idx, num_real_elements_per_tile);
+            CopyIn(i, start_idx, num_real_elements_per_tile);
+            Compute(i, start_idx, num_real_elements_per_tile);
+            CopyOut(i, start_idx, num_real_elements_per_tile);
         }
     }
 
 private:
-    __aicore__ inline void CopyIn(int32_t progress, uint32_t start_idx, uint32_t num_real_elements)
+    __aicore__ inline void CopyIn(int32_t progress, uint32_t start_idx, uint32_t num_real_elements_per_tile)
     {
         AscendC::LocalTensor<DTYPE_X> xLocal = inQueueX.AllocTensor<DTYPE_X>();
         AscendC::LocalTensor<DTYPE_Y> yLocal = inQueueY.AllocTensor<DTYPE_Y>();
-        AscendC::DataCopy(xLocal, xGm[start_idx], num_real_elements);
-        AscendC::DataCopy(yLocal, yGm[start_idx], num_real_elements);
+        AscendC::DataCopy(xLocal, xGm[start_idx], num_real_elements_per_tile);
+        AscendC::DataCopy(yLocal, yGm[start_idx], num_real_elements_per_tile);
         inQueueX.EnQue(xLocal);
         inQueueY.EnQue(yLocal);
     }
-    __aicore__ inline void Compute(int32_t progress, uint32_t start_idx, uint32_t num_real_elements)
+    __aicore__ inline void Compute(int32_t progress, uint32_t start_idx, uint32_t num_real_elements_per_tile)
     {
         AscendC::LocalTensor<DTYPE_X> xLocal = inQueueX.DeQue<DTYPE_X>();
         AscendC::LocalTensor<DTYPE_Y> yLocal = inQueueY.DeQue<DTYPE_Y>();
@@ -53,21 +56,20 @@ private:
         AscendC::LocalTensor<DTYPE_X> bufLocal = buf.Get<DTYPE_X>();
 
 
-        AscendC::Add(zLocal, xLocal, yLocal, num_real_elements);
+        AscendC::Add(zLocal, xLocal, yLocal, num_real_elements_per_tile);
 
         if (AscendC::GetBlockIdx() == 0) {
-            AscendC::printf("progress: %d, start_idx: %u, num_real_elements: %u\n", progress, start_idx, num_real_elements);
-            AscendC::DumpTensor(zLocal, 72, num_real_elements);
+            AscendC::DumpTensor(zLocal, 72, num_real_elements_per_tile);
         }
 
         outQueueZ.EnQue<DTYPE_Z>(zLocal);
         inQueueX.FreeTensor(xLocal);
         inQueueY.FreeTensor(yLocal);
     }
-    __aicore__ inline void CopyOut(int32_t progress, uint32_t start_idx, uint32_t num_real_elements)
+    __aicore__ inline void CopyOut(int32_t progress, uint32_t start_idx, uint32_t num_real_elements_per_tile)
     {
         AscendC::LocalTensor<DTYPE_Z> zLocal = outQueueZ.DeQue<DTYPE_Z>();
-        AscendC::DataCopy(zGm[start_idx], zLocal, num_real_elements);
+        AscendC::DataCopy(zGm[start_idx], zLocal, num_real_elements_per_tile);
         outQueueZ.FreeTensor(zLocal);
     }
 
@@ -83,6 +85,7 @@ private:
     uint32_t num_elements_per_core;
     uint32_t num_tiles;
     uint32_t num_elements_per_tile;
+    uint32_t num_real_elements_per_core;
 };
 
 extern "C" __global__ __aicore__ void heaviside(GM_ADDR x, GM_ADDR y, GM_ADDR z, GM_ADDR workspace, GM_ADDR tiling) {
