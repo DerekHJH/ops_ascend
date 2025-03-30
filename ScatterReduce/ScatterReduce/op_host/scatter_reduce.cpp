bf90865caa12a637e88f26ac0b6cf660e7307242
Junhao Hu <derekhjh9@gmail.com>
2025-03-30 12:10:37 +0800

    Start ScatterReduce

--- ScatterReduce/ScatterReduce/op_host/scatter_reduce.cpp
+++ ScatterReduce/ScatterReduce/op_host/scatter_reduce.cpp
@@ -65,7 +65,7 @@ public:
 
         this->AICore()
             .SetTiling(optiling::TilingFunc);
-        this->AICore().AddConfig("ascend910");
+        this->AICore().AddConfig("ascend910b");
 
     }
 };
